07331fa54f4f6feafce1ca8f8aa4edf355589da4
Blossom Babalola <sapashb@gmail.com>
2020-08-11 13:00:01 +0100

    js done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ class UI {
         this.budgetInput = document.getElementById("budget-input");
         this.budgetWarning = document.getElementById("budget-warning");
         this.budgetOutput = document.getElementById('budget-output')
+        this.expenseOutput = document.getElementById('expenses-output')
         this.expenseForm = document.getElementById('expense-form')
         this.expenseName = document.getElementById('expense-name')
         this.expenseAmt = document.getElementById('expense-amount')
@@ -18,7 +19,7 @@ class UI {
 
     // budget form method
     submitBudgetForm() {
-        console.log(`your budget has been noted`);
+        // console.log(`your budget has been noted`);
         if (this.budgetInput.value === "" || this.budgetInput.value <= 0) {
             this.budgetWarning.classList.remove("d-none");
             const self = this
@@ -35,7 +36,7 @@ class UI {
 
     // balance method
     ShowBalance() {
-        console.log(`this is the balance`)
+        // console.log(`this is the balance`)
         const expense = this.totalExpense()
         this.balance.innerHTML = Number(this.budgetOutput.textContent) - expense
 
@@ -45,19 +46,27 @@ class UI {
             this.balance.style.color = '#ff0000';
         } else if (this.balance.textContent > 0) {
             this.balance.style.color = '#008000';
-            console.log(`the green is working`)
+            //  console.log(`the green is working`)
         }
     }
 
     // total expenses
     totalExpense() {
-        let total = 400
+        let total = 0
+        if (this.itemList.length > 0) {
+            total = this.itemList.reduce((acc, currval) => {
+                //console.log(`this is ths ${acc} and ${currval.amount}`)
+                acc += currval.amount
+                return acc
+            }, 0)
+        }
+        this.expenseOutput.innerHTML = total
         return total
     }
 
     // expense form method
     submitExpenseForm() {
-        console.log(`now expense form is working too`)
+        // console.log(`now expense form is working too`)
         if (this.expenseName.value === '' || this.expenseAmt.value === '' || this.expenseAmt.value <= 0) {
             this.expenseWarning.classList.remove('d-none')
             const self = this
@@ -77,33 +86,72 @@ class UI {
             this.itemID++
             this.itemList.push(expenseObj)
             this.addExpense(expenseObj)
+            this.ShowBalance()
 
             this.expenseName.value = ''
             this.expenseAmt.value = ''
 
-            console.log(expenseObj)
+            // console.log(expenseObj)
         }
     }
 
     // add individual expense 
-
-    addExpense(expenseObj){
-        console.log(`add expense is working`)
+    addExpense(expenseObj) {
+        //  console.log(`add expense is working`)
         const newDiv = document.createElement('div')
         newDiv.classList.add('single-expense')
         newDiv.innerHTML = `
-        <div id="exp-names">${expenseObj.title}</div>
-        <div id="exp-amts">${expenseObj.amount}</div>
-        <div data-id=${expenseObj.id}><i class="fas fa-edit"></i></div>
-        <div data-id=${expenseObj.id}><i class="fas fa-trash"></i></div>
+        <div class='exp-name' id="exp-names">${expenseObj.title}</div>
+        <div class='exp-amt' id="exp-amts">${expenseObj.amount}</div>
+        <div data-id=${expenseObj.id}><i class="fas fa-edit edit-exp"></i></div>
+        <div data-id=${expenseObj.id}><i class="fas fa-trash delete-exp"></i></div>
         `
         this.expenseList.appendChild(newDiv)
     }
+
+    // edit expense
+    editExpense(element) {
+        const el = element.target.parentElement
+        const parent = el.parentElement
+        const id = Number(el.dataset.id)
+        // console.log(el.dataset.id)
+        this.expenseList.removeChild(parent)
+
+        let expense = this.itemList.filter((item) => {
+            return item.id === id
+        })
+        this.expenseName.value = expense[0].title
+        this.expenseAmt.value = expense[0].amount
+
+        let tempList = this.itemList.filter( (item)=> {
+            return item.id !== id
+        })
+        this.itemList = tempList
+        console.log(this.itemList)
+        this.ShowBalance()
+    }
+
+    // delete expense
+    deleteExpense(element) {
+        const el = element.target.parentElement
+        const parent = el.parentElement
+        const id = Number(el.dataset.id)
+        // console.log(el.dataset.id)
+        this.expenseList.removeChild(parent)
+        let tempList = this.itemList.filter( (item)=> {
+            return item.id !== id
+        })
+        this.itemList = tempList
+        console.log(this.itemList)
+        this.ShowBalance()
+    }
+
 }
 
 function eventListeners() {
     const budgetForm = document.getElementById("budget-form");
     const expenseForm = document.getElementById('expense-form')
+    const expenseList = document.getElementById('single-expense')
 
     // instantiating the ui class
     const ui = new UI();
@@ -118,6 +166,15 @@ function eventListeners() {
     expenseForm.addEventListener('submit', function (e) {
         e.preventDefault();
         ui.submitExpenseForm();
+    })
+
+    // edit and delete buttons
+    expenseList.addEventListener('click', (e) => {
+        if (e.target.classList.contains('edit-exp')) {
+            ui.editExpense(e)
+        } else if (e.target.classList.contains('delete-exp')) {
+            ui.deleteExpense(e)
+        }
     })
 
 }
