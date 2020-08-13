fd879db50070cbf01bdaf87d25775f99b9a56ce0
Blossom Babalola <sapashb@gmail.com>
2020-08-13 15:12:43 +0100

    javascript done

--- index.js
+++ index.js
@@ -282,45 +282,6 @@ class UI {
 
     }
 
-    // // display circle chart
-    // displayCircle(expense, income) {
-    //     console.log(`still working`)
-
-    //     // create canvas element
-    //     const canvas = document.createElement('canvas')
-    //     canvas.width = 50;
-    //     canvas.height = 50;
-
-    //     // appending the canvass to the chart
-    //     this.circleChart.appendChild(canvas)
-
-    //     //to draw on canvass, we need to get the context of the canvass
-    //     const ctx = canvas.getContext('2d')
-
-    //     // change the line width
-    //     ctx.lineWidth = 8
-
-    //     // circle radius
-    //     const R = 20
-
-    //     function drawCircle(color, ratio, anticlockwise) {
-    //         console.log(ctx)
-    //         ctx.strokeStyle = color
-    //         ctx.beginPath()
-    //         ctx.arc(canvas.width / 2, canvas.height / 2, R, 0, ratio * 2 * Math.PI, anticlockwise)
-    //         ctx.stroke()
-    //     }
-
-    //     //this.updateChart(this.expense, this.income)
-    //         let ratio = income / (income + expense)
-    //         drawCircle('red', -ratio, true)
-    //         drawCircle('white', 1 - ratio, false)
-    // }
-
-    // update chart: hopefully it works
-
-
-
 
 }
 
@@ -395,12 +356,11 @@ function eventListeners() {
 
 }
 
-//updateChart(1500, 500)
+// updateChart(1500, 500)
 
 document.addEventListener("DOMContentLoaded", function () {
     eventListeners();
 
-    // // circle chart
-    // const interface = new UI();
-    // interface.displayCircle()
+
+
 });
